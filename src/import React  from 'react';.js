fb70c9a52e3d59ef9d0da176b37b87ffd3283c92
matthew bardi <88112650+MatthewBardi@users.noreply.github.com>
2021-09-06 15:58:03 +0000

    making changes

---
+++ src/import React  from 'react';.js
@@ -0,0 +1,20 @@
+import React  from 'react';
+
+
+const Scrolls = (props) =>  {
+
+			return(
+
+							<div style={{overflow:'scroll', border:'5px solid black', height:'500px'}}>
+
+
+										{props.children}
+
+							</div>
+
+							);
+
+						};
+
+
+export default Scrolls
